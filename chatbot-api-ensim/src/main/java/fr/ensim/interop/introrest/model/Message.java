8218d8fb702180f5503493e0a207f9f57fdf50b5
thatwasyahya <98318509+thatwasyahya@users.noreply.github.com>
2024-05-28 14:38:30 +0200

    Add files via upload
    message

---
+++ chatbot-api-ensim/src/main/java/fr/ensim/interop/introrest/model/Message.java
@@ -0,0 +1,55 @@
+package fr.ensim.interop.introrest.model.telegram;
+
+public class Message {
+    private String text;
+    private Chat chat;
+    private From from;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Chat getChat() {
+        return chat;
+    }
+
+    public void setChat(Chat chat) {
+        this.chat = chat;
+    }
+
+    public From getFrom() {
+        return from;
+    }
+
+    public void setFrom(From from) {
+        this.from = from;
+    }
+
+    public static class Chat {
+        private Long id;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+    }
+
+    public static class From {
+        private String firstName;
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+    }
+}
